114e61b2783d53b262e563c366f5c7b66c3bed5b
Ugochidev <ugochukwuchioma16@gmail.com>
2022-03-16 12:29:14 +0100

    updatin

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import express from "express";
-import socket from "socket.io";
+const express = require("express");
+const socket = require("socket.io");
 // app setup
-const app = express()
-const server = app.listen(4768, function(){
-    console.log("listening to requests on port 4768");
-})
+const app = express();
+const server = app.listen(process.env.PORT, function () {
+  console.log("listening to requests on PORT");
+});
 
 // Static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //  socket setup
 const io = socket(server);
-io.on('connection',function(socket){
-    console.log('made socket connection', socket.id);
+io.on("connection", function (socket) {
+  console.log("made socket connection", socket.id);
 
-    socket.on('chat', function(data){
-        io.sockets.emit('chat', data);
-    })
-    socket.on("typing", function(data){
-        socket.broadcast.emit("typing", data)
-    })
-})
+  socket.on("chat", function (data) {
+    io.sockets.emit("chat", data);
+  });
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
