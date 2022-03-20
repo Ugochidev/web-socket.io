bd4913e939b4bc812de29d50d232913bcc08cfa8
Ugochidev <ugochukwuchioma16@gmail.com>
2022-03-20 20:16:35 +0100

    feat/modified chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,24 @@
 // make connection
-const socket = io.connect("https://ugochidev-web.herokuapp.com/");
+// const socket = io.connect("https://ugochidev-web.herokuapp.com/");
+
+let newUserPass = prompt(
+  "App locked, Please input App Password."
+).toLowerCase();
+if (
+  newUserPass == "myown_" ||
+  newUserPass == "josh_" ||
+  newUserPass == "ugochidev_" ||
+  newUserPass == "wheeler_"
+) {
+  alert("Welcome in, Enjoy your sesion.....");
+  socket = io.connect("https://ugochidev-web.herokuapp.com/");
+} else {
+  alert("Wrong Password");
+  const chat = document.getElementById("mario-chat");
+  chat.innerHTML =
+    "<p><strong>Please get a valid password and reload.</strong></p>";
+  chat.style.background = "#e0b0b0";
+}
 // Query DOM
 const message = document.getElementById("message");
 handle = document.getElementById("handle");
@@ -8,10 +27,19 @@ output = document.getElementById("output");
 feedback = document.getElementById("feedback");
 //   Emit events
 btn.addEventListener("click", function () {
+  if (handle.value == null || handle.value == "") {
+    alert("Pls input a handle");
+    return null;
+  }
+  if (message.value == null || message.value == "") {
+    alert("Cannot send blank message!");
+    return null;
+  }
   socket.emit("chat", {
     message: message.value,
     handle: handle.value,
   });
+   message.value = "";
 });
 message.addEventListener("keypress", function () {
   socket.emit("typing", handle.value);
